3e08fa3bcb60efa3b37d5494ad10fea0390a59cf
Rishav mondal <58592749+rmondal37@users.noreply.github.com>
2021-07-19 13:19:00 +0530

    initial commit

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpenseFilter";
+import "./Expenses.css";
+import "./ExpensesList.css";
+import ExpenseChart from "./ExpensesChart";
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  let expensesContent = (
+    <h2 className="expenses-list__fallback">No expenses found.</h2>
+  );
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpenseChart expenses={filteredExpenses}></ExpenseChart>
+        {expensesContent}
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
